75e6e243419781b8a279354cfab17c2651e2ec5a
Asha2805 <43291441+Asha2805@users.noreply.github.com>
2018-09-26 07:59:02 -0400

    Update vowel.java

--- vowel.java
+++ vowel.java
@@ -6,7 +6,7 @@ public static void main(String[] args)
 char q;
 Scanner s=new Scanner(System.in);
 q=s.next().charAt(0);
-if(q=='a'||q=='e'||q=='i'||q=='o'||q=='u')
+if(q=='a'&&q=='A'||q=='e'&&q=='E'||q=='i'&&q=='I'||q=='o'&&q=='O'||q=='u'&&q=='U')
 {
 System.out.println("Vowel");
 }
